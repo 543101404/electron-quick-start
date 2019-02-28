f5292552c87ab44b9539a4c0e154e19a0aeaf2d4
bitao <bitao@chanjet.com>
2019-02-28 13:38:40 +0800

    electron build 之前配置

---
+++ build64config.bat
@@ -0,0 +1,6 @@
+set npm_config_target=4.0.4
+set npm_config_arch=x64
+set npm_config_target_arch=x64
+set npm_config_disturl=https://gh-contractor-zcbenz.s3.amazonaws.com/atom-shell
+set npm_config_runtime=electron
+set npm_config_build_from_source=true
