e8a53e33ed84ce841d906cfcd88475716aa6f015
bitao <bitao@chanjet.com>
2019-02-28 15:16:24 +0800

    32位调用成功

--- build32config.bat
+++ build32config.bat
@@ -4,3 +4,8 @@ set npm_config_target_arch=ia32
 set npm_config_disturl=https://gh-contractor-zcbenz.s3.amazonaws.com/atom-shell
 set npm_config_runtime=electron
 set npm_config_build_from_source=true
+npm config set python D:\Python27x86\python.exe
+npm run rebuild32
+
+
+
--- build64config.bat
+++ build64config.bat
@@ -4,3 +4,4 @@ set npm_config_target_arch=x64
 set npm_config_disturl=https://gh-contractor-zcbenz.s3.amazonaws.com/atom-shell
 set npm_config_runtime=electron
 set npm_config_build_from_source=true
+npm config set python D:\Python27\python.exe
