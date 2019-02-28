abcb25f9e8afd705ba3919c6abc50c1d31969977
bitao <bitao@chanjet.com>
2019-02-28 13:31:51 +0800

    64位ok

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 // Modules to control application life and create native browser window
 const {app, BrowserWindow} = require('electron')
+var ffi = require('./node_modules/ffi/ffi.node')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -14,6 +15,8 @@ function createWindow () {
       nodeIntegration: true
     }
   })
+  
+  mainWindow.webContents.openDevTools()
 
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
