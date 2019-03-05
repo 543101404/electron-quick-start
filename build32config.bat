e56346186e11ae9d432afea6593544e1163065f2
bitao <bitao@chanjet.com>
2019-03-05 16:22:41 +0800

    111

--- build32config.bat
+++ build32config.bat
@@ -5,7 +5,7 @@ set npm_config_disturl=https://gh-contractor-zcbenz.s3.amazonaws.com/atom-shell
 set npm_config_runtime=electron
 set npm_config_build_from_source=true
 
-npm install electron@4.0.4 --arch=ia32
+npm install electron@4.0.4 --arch=ia32 -abi=69
 npm run rebuild32
 
 
